ffa5261d228b9975a1f63d086ce315e4bb8ae820
shaiArnon <sharnon1@gmail.com>
2017-12-22 16:16:04 +0200

    Rate limiter v1

--- src/RateLimiter.java
+++ src/RateLimiter.java
@@ -1,10 +1,18 @@
 /**
  * A token bucket based rate-limiter.
- *
+ * <p>
  * This class should implement a "soft" rate limiter by adding maxBytesPerSecond tokens to the bucket every second,
  * or a "hard" rate limiter by resetting the bucket to maxBytesPerSecond tokens every second.
  */
+import java.util.concurrent.*;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit.*;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class RateLimiter implements Runnable {
+
     private final TokenBucket tokenBucket;
     private final Long maxBytesPerSecond;
 
@@ -14,7 +22,18 @@ public class RateLimiter implements Runnable {
     }
 
     @Override
+    /**
+     * "Hard" rate limiter implementation
+     */
     public void run() {
-        //TODO
+        while(true) {
+            try {
+                this.tokenBucket.set(maxBytesPerSecond);
+                Thread.sleep(1000); // Adding BPS to the token bucket every second
+            } catch (InterruptedException e) {
+                System.err.println("Download failed:" + e);
+            }
+        }
     }
-}
+
+    }
